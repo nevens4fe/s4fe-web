3e0b9d301a55b5843bb73cead5ad248fa9d6fee1
Shone Radulovic <neven@s4fe.io>
2020-11-10 13:02:48 +0100

    Updated google api key

--- src/boot/hello.js
+++ src/boot/hello.js
@@ -3,7 +3,7 @@ import hello from 'hellojs'
 export default ({ Vue }) => {
   hello.init({
     facebook: '151707949234327',
-    google: 'afsg25yh2h'
+    google: '689677381423-1cb7ejukrpjm3cckk0rmc8pfmftdkp4a.apps.googleusercontent.com'
   })
 
   Vue.prototype.$hello = hello
